910d4c428b02732f9cbe4a8b3d620876d3129a74
evanmah88 <112901221+evanmah88@users.noreply.github.com>
2023-11-10 10:09:04 -0700

    page updated, card components added

--- app/page.js
+++ app/page.js
@@ -1,9 +1,38 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
     <main>
-      ..start
+      <h1>Home</h1>
+<div className="container mx-auto">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra, sem vitae interdum consectetur, sem nisi bibendum nisi, in tristique neque urna id tellus. Pellentesque at facilisis arcu. Nam efficitur enim quam, rutrum dignissim erat venenatis a. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi lobortis est non tellus dignissim sollicitudin. Ut non nisl leo. Sed sed enim tortor. Suspendisse molestie sem eu pellentesque gravida. Quisque scelerisque, nunc eu lacinia porttitor, velit sem finibus arcu, at scelerisque enim metus vitae justo.
+
+Mauris ut aliquam eros. Cras venenatis rhoncus pharetra. Maecenas dapibus lorem purus, sed volutpat mauris luctus a. Ut eget nunc eu odio tempus blandit. Sed quis ornare ex, nec porta lacus. Donec in velit rutrum, maximus orci eget, tristique urna. Integer vitae est urna. Proin lobortis, velit vitae molestie suscipit, mi ex dignissim nisi, ac pellentesque mauris enim nec eros. Praesent hendrerit, nibh lacinia bibendum accumsan, mi mauris sagittis metus, vitae auctor erat libero vitae tellus. Curabitur eget accumsan dui, sagittis molestie dui. Sed auctor volutpat sollicitudin. Sed mauris ligula, auctor id tempor sed, cursus et leo.
+
+Maecenas accumsan pharetra cursus. Aenean in ipsum nec purus lobortis consectetur. Nunc ut nisi nec ante viverra cursus. Phasellus scelerisque, enim ac mollis malesuada, lorem lorem rutrum odio, eget vehicula nulla arcu elementum enim. Sed vel tortor scelerisque, auctor felis vitae, dapibus tortor. Ut ac neque ac est condimentum posuere at ac velit. Mauris non maximus dui.
+
+Nunc in pharetra ex. Aenean ullamcorper ligula libero, eu vestibulum eros sagittis eu. Cras a diam sed ex egestas fringilla tincidunt a magna. Nunc semper blandit egestas. Morbi non nulla id velit blandit blandit a sit amet ante. Donec iaculis risus ligula, eu hendrerit urna mattis in. Mauris suscipit lorem eget pretium imperdiet. Suspendisse eu ipsum mattis, tristique nisi et, dictum sem. Cras consectetur auctor sem eget rhoncus. Sed imperdiet commodo nulla at commodo. Vestibulum ullamcorper nulla sed quam molestie sagittis pretium a ligula. Mauris eget tortor vitae orci convallis egestas</p>
+</div>
+      
+
+<div className="container mx-auto">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra, sem vitae interdum consectetur, sem nisi bibendum nisi, in tristique neque urna id tellus. Pellentesque at facilisis arcu. Nam efficitur enim quam, rutrum dignissim erat venenatis a. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi lobortis est non tellus dignissim sollicitudin. Ut non nisl leo. Sed sed enim tortor. Suspendisse molestie sem eu pellentesque gravida. Quisque scelerisque, nunc eu lacinia porttitor, velit sem finibus arcu, at scelerisque enim metus vitae justo.
+
+Mauris ut aliquam eros. Cras venenatis rhoncus pharetra. Maecenas dapibus lorem purus, sed volutpat mauris luctus a. Ut eget nunc eu odio tempus blandit. Sed quis ornare ex, nec porta lacus. Donec in velit rutrum, maximus orci eget, tristique urna. Integer vitae est urna. Proin lobortis, velit vitae molestie suscipit, mi ex dignissim nisi, ac pellentesque mauris enim nec eros. Praesent hendrerit, nibh lacinia bibendum accumsan, mi mauris sagittis metus, vitae auctor erat libero vitae tellus. Curabitur eget accumsan dui, sagittis molestie dui. Sed auctor volutpat sollicitudin. Sed mauris ligula, auctor id tempor sed, cursus et leo.
+
+Maecenas accumsan pharetra cursus. Aenean in ipsum nec purus lobortis consectetur. Nunc ut nisi nec ante viverra cursus. Phasellus scelerisque, enim ac mollis malesuada, lorem lorem rutrum odio, eget vehicula nulla arcu elementum enim. Sed vel tortor scelerisque, auctor felis vitae, dapibus tortor. Ut ac neque ac est condimentum posuere at ac velit. Mauris non maximus dui.
+
+Nunc in pharetra ex. Aenean ullamcorper ligula libero, eu vestibulum eros sagittis eu. Cras a diam sed ex egestas fringilla tincidunt a magna. Nunc semper blandit egestas. Morbi non nulla id velit blandit blandit a sit amet ante. Donec iaculis risus ligula, eu hendrerit urna mattis in. Mauris suscipit lorem eget pretium imperdiet. Suspendisse eu ipsum mattis, tristique nisi et, dictum sem. Cras consectetur auctor sem eget rhoncus. Sed imperdiet commodo nulla at commodo. Vestibulum ullamcorper nulla sed quam molestie sagittis pretium a ligula. Mauris eget tortor vitae orci convallis egestas</p>
+</div>
+
+<div className="container mx-auto">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra, sem vitae interdum consectetur, sem nisi bibendum nisi, in tristique neque urna id tellus. Pellentesque at facilisis arcu. Nam efficitur enim quam, rutrum dignissim erat venenatis a. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi lobortis est non tellus dignissim sollicitudin. Ut non nisl leo. Sed sed enim tortor. Suspendisse molestie sem eu pellentesque gravida. Quisque scelerisque, nunc eu lacinia porttitor, velit sem finibus arcu, at scelerisque enim metus vitae justo.
+
+Mauris ut aliquam eros. Cras venenatis rhoncus pharetra. Maecenas dapibus lorem purus, sed volutpat mauris luctus a. Ut eget nunc eu odio tempus blandit. Sed quis ornare ex, nec porta lacus. Donec in velit rutrum, maximus orci eget, tristique urna. Integer vitae est urna. Proin lobortis, velit vitae molestie suscipit, mi ex dignissim nisi, ac pellentesque mauris enim nec eros. Praesent hendrerit, nibh lacinia bibendum accumsan, mi mauris sagittis metus, vitae auctor erat libero vitae tellus. Curabitur eget accumsan dui, sagittis molestie dui. Sed auctor volutpat sollicitudin. Sed mauris ligula, auctor id tempor sed, cursus et leo.
+
+Maecenas accumsan pharetra cursus. Aenean in ipsum nec purus lobortis consectetur. Nunc ut nisi nec ante viverra cursus. Phasellus scelerisque, enim ac mollis malesuada, lorem lorem rutrum odio, eget vehicula nulla arcu elementum enim. Sed vel tortor scelerisque, auctor felis vitae, dapibus tortor. Ut ac neque ac est condimentum posuere at ac velit. Mauris non maximus dui.
+
+Nunc in pharetra ex. Aenean ullamcorper ligula libero, eu vestibulum eros sagittis eu. Cras a diam sed ex egestas fringilla tincidunt a magna. Nunc semper blandit egestas. Morbi non nulla id velit blandit blandit a sit amet ante. Donec iaculis risus ligula, eu hendrerit urna mattis in. Mauris suscipit lorem eget pretium imperdiet. Suspendisse eu ipsum mattis, tristique nisi et, dictum sem. Cras consectetur auctor sem eget rhoncus. Sed imperdiet commodo nulla at commodo. Vestibulum ullamcorper nulla sed quam molestie sagittis pretium a ligula. Mauris eget tortor vitae orci convallis egestas</p>
+</div>
     </main>
   )
 }
---
+++ components/card/itemCard.jsx
@@ -0,0 +1,12 @@
+ function ItemCard ( {children}){
+    return (
+        <>
+        <p>Item Card Component</p>
+        {children}
+        </>
+    )
+ }
+
+
+
+ export {ItemCard}
